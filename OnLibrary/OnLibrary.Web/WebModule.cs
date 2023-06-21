b45b1ea68c211c8e591e38abe27df3c74d5faca2
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-06-22 00:04:00 +0600

    6. Added WebModule for registering types to container for DI

---
+++ OnLibrary/OnLibrary.Web/WebModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace OnLibrary.Web
+{
+    public class WebModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
