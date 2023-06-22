284950e0e1d3ffb0eb1228dbf9239a0f0b553374
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-06-22 16:55:43 +0600

    Updated namespaces of moved ApplicationDbContext

--- OnLibrary/OnLibrary.Web/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ OnLibrary/OnLibrary.Web/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -3,9 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using OnLibrary.Web.Data;
-using System;
+using OnLibrary.Persistence;
 
 namespace OnLibrary.Web.Data.Migrations
 {
--- OnLibrary/OnLibrary.Web/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ OnLibrary/OnLibrary.Web/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -2,9 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using OnLibrary.Web.Data;
-using System;
+using OnLibrary.Persistence;
 
 namespace OnLibrary.Web.Data.Migrations
 {
