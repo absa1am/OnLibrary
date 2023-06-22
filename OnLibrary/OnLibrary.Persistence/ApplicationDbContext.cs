9dcb14d92d0c95eb52dd011f1264fb51879ab0ce
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-06-22 16:26:04 +0600

    Added ApplicationDbContext for SQL server communication

---
+++ OnLibrary/OnLibrary.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnLibrary.Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext, IApplicationDbContext
+    {
+        private readonly string _connectionString;
+        private readonly string _migrationAssembly;
+
+        public ApplicationDbContext(string connectionString, string migrationAssembly)
+        {
+            _connectionString = connectionString;
+            _migrationAssembly = migrationAssembly;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString, option => option.MigrationsAssembly(_migrationAssembly));
+            }
+        }
+    }
+}
