0f6145794ab52f4bed5639c0bff314577f3772da
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-06-22 17:37:08 +0600

    Implemented IUnitOfWork signature in abstract UnitOfWork

---
+++ OnLibrary/OnLibrary.Persistence/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnLibrary.Domain.UnitOfWorks;
+
+namespace OnLibrary.Persistence
+{
+    public abstract class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual void Dispose()
+        {
+            _dbContext?.Dispose();
+        }
+
+        public async virtual Task DisposeAsync()
+        {
+            _dbContext?.DisposeAsync();
+        }
+
+        public virtual void Save()
+        {
+            _dbContext?.SaveChanges();
+        }
+
+        public virtual async Task SaveAsync()
+        {
+            _dbContext?.SaveChangesAsync();
+        }
+    }
+}
