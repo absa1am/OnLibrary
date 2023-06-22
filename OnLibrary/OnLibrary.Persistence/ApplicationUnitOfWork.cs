1170dc93335537d1bbaf8c5d4807f3fc88228b08
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-06-22 17:44:02 +0600

    Added ApplicationUnitOfWork

---
+++ OnLibrary/OnLibrary.Persistence/ApplicationUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnLibrary.Application;
+
+namespace OnLibrary.Persistence
+{
+    public class ApplicationUnitOfWork : UnitOfWork, IApplicationUnitOfWork
+    {
+        public ApplicationUnitOfWork(IApplicationDbContext dbContext) : base((DbContext) dbContext)
+        {
+
+        }
+    }
+}
