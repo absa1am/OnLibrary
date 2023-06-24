6220ad8537da7215d9e05aff0fe85fb4295065ed
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-06-24 16:39:08 +0600

    Added Registration action and view

---
+++ OnLibrary/OnLibrary.Web/Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnLibrary.Persistence.Features.Membership;
+using OnLibrary.Web.Models;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+
+namespace OnLibrary.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILifetimeScope _scope;
+        private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(ILifetimeScope scope, ILogger<AccountController> logger,
+            SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _scope = scope;
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RegisterAsync(string returnUrl = null)
+        {
+            var model = _scope.Resolve<RegisterModel>();
+
+            model.ReturnUrl = returnUrl;
+
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegisterAsync(RegisterModel model)
+        {
+            model.ReturnUrl ??= Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = userId, code = code, returnUrl = model.ReturnUrl },
+                        protocol: Request.Scheme);
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToAction("RegisterConfirmation", new { email = model.Email, returnUrl = model.ReturnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+
+                        return LocalRedirect(model.ReturnUrl);
+                    }
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+    }
+}
