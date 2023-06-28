6a3f90edad0fda7160867eb205dbbf9149b63cec
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-06-29 03:19:08 +0600

    Added new feature

--- OnLibrary/OnLibrary.Domain/Entities/Publication.cs
+++ OnLibrary/OnLibrary.Domain/Entities/Publication.cs
@@ -4,5 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
+        public List<Book> Books { get; set; }
     }
 }
