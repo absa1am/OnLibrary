193bf8af1f4734292264d2144b7b9e8308bbc529
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-06-29 03:21:36 +0600

    Added Book entity

---
+++ OnLibrary/OnLibrary.Domain/Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace OnLibrary.Domain.Entities
+{
+    public class Book : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public Guid PublicationId { get; set; }
+        public Publication Publication { get; set; }
+        public List<BookAuthor> Authors { get; set; }
+    }
+}
