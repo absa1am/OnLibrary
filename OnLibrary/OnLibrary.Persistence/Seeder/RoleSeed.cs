917fe2a3bdc3faafd35dee782ff5e32cc5ea295a
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-09-22 00:29:48 +0600

    Added RoleSeed for seeding roles

---
+++ OnLibrary/OnLibrary.Persistence/Seeder/RoleSeed.cs
@@ -0,0 +1,29 @@
+﻿using OnLibrary.Persistence.Features.Membership;
+
+namespace OnLibrary.Persistence.Seeder
+{
+    public static class RoleSeed
+    {
+        public static IList<ApplicationRole> Roles()
+        {
+            return new List<ApplicationRole>
+            {
+                new ApplicationRole
+                {
+                    Id = new Guid("6505EC93-B8EE-418B-A3D7-13FC3E3FFC96"),
+                    Name = "Admin"
+                },
+                new ApplicationRole
+                {
+                    Id = new Guid("459A979D-8358-4EBB-9758-D8AA35D7C7FF"),
+                    Name = "Manager"
+                },
+                new ApplicationRole
+                {
+                    Id = new Guid("0E7949B4-0A2B-43F7-9DDC-E8822A4B60C0"),
+                    Name = "User"
+                }
+            };
+        }
+    }
+}
